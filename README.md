6c563114914330b77948fbc082c2c0026aa4c2a4
cripboy <cripboygit@gmail.com>
2022-03-02 21:52:29 -0300

    adding example and fixing old file open

--- README.md
+++ README.md
@@ -1,99 +1,14 @@
-# love2d ASE loader
+# Lua asprite loader
+[It](ase-loader.lua) helps you to read ASE files without parsing them into a json or something. So helpful for development,
+but I recommend not using it in production (without an atlas builder), loading files this way is
+slower than just passing src :smile: data.
 
-Helps you to read ASE files without parsing them in a json or something. So useful for development, but I recommend not using it in production, loading files this way is slower than just passing src data and this loader doesn't use third-party libs, everything here is written in pure LUA, so no use it in production :smile:. You can find more details [here](https://github.com/aseprite/aseprite/blob/master/docs/ase-file-specs.md).
+### Example:
+You will find an example in [main.lua](main.lua) of how to make a simple sprite object with
 
-### example:
+Note: this loader is not an animation framework, it provides a way to load files from aseprite. Implement
+your own sprite system based on the example
 
-```lua
-local loader = require "loader"
-
--- pixelart preset
-love.graphics.setDefaultFilter("nearest", "nearest")
-
--- load file
-local player_ase = loader("player.ase")
-
--- get chunk 1 from frame 5
-local chunk = player_ase.header.frames[5].chunks[1].data
-
--- decompress image bytes
-local buffer = love.data.decompress("data", "zlib", chunk.data)
-
--- parse it into a love2d image
-local img_data = love.image.newImageData(chunk.width, chunk.height, "rgba8", buffer)
-local image = love.graphics.newImage(img_data)
-
-function love.draw()
-	love.graphics.draw(image, 0, 0, 0, 10, 10)
-end 
-```
-
-### ouput structure:
-
-```lua
-local ase = {
-  header = {
-    file_size = int,
-    magic_number = int, -- (need to be 0xA5E0)
-    frames_number = int,
-    width = int,
-    height = int,
-    color_depth = int,
-    opacity = int,
-    speed = int,
-    palette_entry = int,
-    number_color = int,
-    pixel_width = int,
-    pixel_height = int,
-    grid_x = int,
-    grid_y = int,
-    grid_width = int,
-    grid_height = int,
-    frames = {} -- (array of frame)
-  }
-}
-
-local frame = {
-  bytes_size = int,
-  magic_number = int, -- (need to be 0xF1FA)
-  frame_duration = int,
-  chunks_number = int,
-  
-   -- (array of chunk)
-  chunks = {
-    type = int,
-    
-    -- the data will be defined by the chunk type
-    data = {}
-  }
-}
-
--- chunk can have multiple types you can see it on code 😜, but i'll show the most essentials
-
--- type: 0x2005
-local chunk_cel_data = {
-  layer_index = int,
-  x = int,
-  y = int,
-  opacity_level = int,
-  type = int, -- (only type 2 are acepted)
-  width = int,
-  height = int,
-  data = string -- (Base64 string, this is the frame image compressed)
-}
-
--- type: 0x2018
-local chunk_tags_data = {
-  number = int,
-  
-  -- array of tag (this inside is just anexample)
-  tags = {
-    {
-      from = int,
-      to = int,
-      direction = int,
-      name = string
-    }
-  }
-}
-```
+### Loader output:
+The output is the same from [ase file specs docs](https://github.com/aseprite/aseprite/blob/master/docs/ase-file-specs.md) with
+few changes. You can easily find it at [ase-loader.lua](ase-loader.lua)
--- main.lua
+++ main.lua
@@ -1,13 +1,107 @@
-local loader = require "loader"
+-- example
+local loader = require "ase-loader"
+local sprite = {}
 
-love.graphics.setDefaultFilter("nearest", "nearest")
+function sprite:load(dir)
+	local file = loader(dir)
 
-local player_ase = loader("player.ase")
-local chunk = player_ase.header.frames[5].chunks[1].data
-local buffer = love.data.decompress("data", "zlib", chunk.data)
-local img_data = love.image.newImageData(chunk.width, chunk.height, "rgba8", buffer)
-local image = love.graphics.newImage(img_data)
+	-- size
+	self.width = file.header.width
+	self.height = file.header.height
+	self.frames = {}
+	self.tags = {}
+	self.active = "" -- active tag
+	self.index = 1 -- actual frame
+	self.time = 0 -- current time elapsed
+
+	for _, frame in ipairs(file.header.frames) do
+		for _, chunk in ipairs(frame.chunks) do
+			-- frame image data
+			if chunk.type == 0x2005 then
+				local cel = chunk.data
+				local buffer = love.data.decompress("data", "zlib", cel.data)
+				local data = love.image.newImageData(cel.width, cel.height, "rgba8", buffer)
+				local image = love.graphics.newImage(data)
+				local canvas = love.graphics.newCanvas(self.width, self.height)
+
+				-- you need to draw in a canvas before.
+				-- frame images can be of different sizes
+				-- but never bigger than the header width and height
+				love.graphics.setCanvas(canvas)
+				love.graphics.draw(image, cel.x, cel.y)
+				love.graphics.setCanvas()
+
+				table.insert(self.frames, {
+					image = canvas,
+					duration = frame.frame_duration / 1000})
+
+			-- tag
+			elseif chunk.type == 0x2018 then
+				for i, tag in ipairs(chunk.data.tags) do
+					-- first tag as default
+					if i == 1 then
+						self.active = tag.name
+					end
+
+					-- aseprite use 0 notation to begin
+					-- but in lua, everthing starts in 1
+					tag.to = tag.to + 1
+					tag.from = tag.from + 1
+					tag.frames = tag.to - tag.from
+					self.tags[tag.name] = tag
+				end
+			end
+		end
+	end
+end
+
+function sprite:play(name)
+	assert(self.tags[name], "invalid tag: " .. name)
+
+	-- if arent playing...
+	-- prevent indexes bigger than tag to
+	if self.active ~= name then
+		self.index = self.tags[name].from
+	end
+
+	self.active = name
+end
+
+function sprite:update(delta)
+	assert(self.active, "no tag playing, sure you set this in aseprite?")
+	local tag = self.tags[self.active]
+
+	-- time tracker are useless on single frames...
+	if (tag.to - tag.from) ~= 0 then
+		self.time = self.time + delta
+
+		-- next frame
+		if self.time >= self.frames[self.index].duration then
+			self.index = self.index + 1
+			self.time = 0 -- you can change to "self.time - frame.duration" as well
+
+			-- reach the end, return to begin
+			if self.index > tag.to then
+				self.index = tag.from
+			end
+		end
+	end
+end
+
+function sprite:draw(x, y, angle, sx, sy)
+	love.graphics.draw(self.frames[self.index].image, x, y, angle, sx, sy)
+end
+
+function love.load()
+	love.graphics.setDefaultFilter("nearest", "nearest")
+	sprite:load("player.ase")
+	sprite:play("run")
+end
+
+function love.update(delta)
+	sprite:update(delta)
+end
 
 function love.draw()
-	love.graphics.draw(image, 0, 0, 0, 10, 10)
+	sprite:draw(0, 0, 0, 10, 10)
 end
